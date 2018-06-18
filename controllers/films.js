b21bf16a8c57a3787af34cbf4287736621124a97
Daniel Childs <dc.1994@outlook.com>
2018-06-18 16:46:22 +0100

    can view all films

--- controllers/films.js
+++ controllers/films.js
@@ -1,3 +1,12 @@
 const films = require('../models/films')();
 const Film = require('../models/film');
 const Review = require('../models/review');
+const express = require("express")
+
+const filmsRouter = new express.Router();
+
+filmsRouter.get('/', function(req, res){
+  res.json(films);
+})
+
+module.exports = filmsRouter;
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = new express.Router();
 
-router.use('/films', require('./films.js'))
+router.use('/film', require('./films.js'))
+
+router.get('/', function(req, res){
+  res.json({data: 'Hello, World!'});
+});
+
+
+
 
 module.exports = router;
